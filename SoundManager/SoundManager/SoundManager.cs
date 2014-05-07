aad1e6ac1e50f79b7d4b1c6855ef187b15e6adcc
Flowers61 <flores.eric94@gmail.com>
2014-05-06 22:15:03 -0700

    Soundmanager

---
+++ SoundManager/SoundManager/SoundManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SoundManager
+{
+    public class SoundManager
+    {
+        private float _volume;
+        private bool _play = false;
+        private List<Song> _MUSIC;
+        private List<SoundEffect> _SOUNDEFFECTS;
+
+        public SoundEffect soundEffect;
+        public Song music;
+        public bool play { get { return _play; } set { _play = value; } }
+
+        public SoundManager()
+        {
+            soundEffect = null;
+            music = null;
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            music = Content.Load<Song>("Music/JW86 -  REBOOT-INDONESIA - 03 Magical Rainbow Castle");
+        }
+
+        public void Play()
+        {
+            MediaPlayer.Play(music);
+            MediaPlayer.IsRepeating = true;
+            
+        }
+        public void isPlay()
+        {
+            if (!_play)
+            {
+                _play = true;
+                Play();
+            }
+
+        }
+    }
+}
